d2a235429faa98e8ec9140a56e63cab4c4de8af6
Kristian Perez <kpere192@fiu.edu>
2024-05-24 16:09:45 -0400

    User show page now shows what events the user is going to/has attended

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :created_events, inverse_of: "creator", class_name: "Event"
+  has_many :attendings, foreign_key: :event_attendee_id
   has_many :attended_events, through: "attendings"
 end

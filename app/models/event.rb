259d14937b32080aaca114e957b4079d8c0f4923
Kristian Perez <kpere192@fiu.edu>
2024-05-25 13:52:55 -0400

    Added validators for event variables, and added error msgs to the new/edit form

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ApplicationRecord
     belongs_to :creator, class_name: "User", foreign_key: "creator_id"
     has_many :attendings, foreign_key: :attended_event_id, dependent: :destroy
     has_many :attendees, through: :attendings, source: :event_attendee, dependent: :destroy
+
+    validates :title, :place, :date, :time, presence: true  
     
     scope :past, -> { where("date < ?", Date.today) }
     scope :upcoming, -> { where("date > ?", Date.today) }
